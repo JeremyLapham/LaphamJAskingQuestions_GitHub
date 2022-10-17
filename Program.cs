4f0db635fa56da8e3981a7a2a0f4fc4ea8acd3ef
JeremyLapham <jeremylapham2004@gmail.com>
2022-10-17 15:16:55 -0700

    Asking Questions

--- Program.cs
+++ Program.cs
@@ -6,15 +6,26 @@
 
 
 bool playAgain = true;
+bool whyNumber = true;
+bool whyNumber2 = true;
+int needNum1 = 0;
+
 while(playAgain)
 {
 Console.WriteLine("Hello, what is your name?");
 string youName = Console.ReadLine();
-
-Console.WriteLine("Hey " + youName + " What time did you wake up?");
+whyNumber = Int32.TryParse(youName, out needNum1);
+if(whyNumber == true)
+{
+    Console.WriteLine("I'm pretty sure most names aren't just numbers but who am I to judge.");
+}
+else if(whyNumber == false)
+{
+Console.WriteLine("Hey " + youName + ". What time did you wake up?");
 string wakeUp = Console.ReadLine();
 
 Console.WriteLine("Damn bro you woke up at " + wakeUp + " that's insane");
+}
 
 Console.WriteLine("Do you want to play again? Type yes to play again type end to stop");
 string playAgain2 = Console.ReadLine().ToLower();
